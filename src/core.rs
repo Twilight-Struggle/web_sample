f4fe8479e7101bc476e11b3cc82abe222a967d02
Twilight-Struggle <pikupiken@yahoo.co.jp>
2022-01-02 23:29:22 +0900

    ボードゲームのコアロジック作成

---
+++ src/core.rs
@@ -0,0 +1,32 @@
+#[derive(Clone)]
+pub struct Board {
+    cells: Vec<bool>
+}
+
+impl Board {
+    pub fn new() -> Board {
+        Board {
+            cells: vec![true, false, false]
+        }
+    }
+    pub fn reset(&self) -> Board {
+        Board {
+            cells: vec![true, false, false]
+        }
+    }
+    pub fn idou(&self, from: usize, to: usize) -> Option<Board> {
+        if self.cells[from] {
+            None
+        }
+        else {
+            if to - from == 1 && 2 >= from && 2 >= to {
+                let mut cells = vec![false, false, false];
+                cells[to] = true;
+                Some(Board {cells: cells})
+            }
+            else {
+                None
+            }
+        }
+    }
+}
