9b4ea45c3abe9def7235a842e7d7f01a6e887048
Twilight-Struggle <pikupiken@yahoo.co.jp>
2022-01-02 20:46:35 +0900

    resetをとりあえず実装

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,29 @@
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::{post, web, App, HttpResponse, HttpServer};
 use std::net::TcpListener;
+use serde::{Serialize, Deserialize};
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+#[derive(Serialize, Deserialize)]
+pub struct Board {
+    pub cells: Vec::<bool>
+}
+
+#[post("/reset")]
+async fn reset() -> HttpResponse {
+    let board = Board { cells: vec![true, false, false] };
+    HttpResponse::Ok().json(board)
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .service(reset)
+        })
         .listen(listener)?
         .run();
     Ok(server)
