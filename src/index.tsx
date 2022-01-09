d3328f8ddc9cea1f30622c5eca067ba01795f263
Twilight-Struggle <pikupiken@yahoo.co.jp>
2022-01-10 03:06:19 +0900

    resetを押したときAPIを叩くように変更

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,16 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Board() {
+type BoardPorps = {boardsta: boolean[]}
+function Board(props: BoardPorps) {
   return (
     <div>
       <div className='board-row'>
-        <button className='square'></button>
-        <button className='square'></button>
-        <button className='square'></button>
+        {props.boardsta.map(
+          (piece, index) => piece ?
+            <button key={index} className='square-piece'></button>:
+            <button key={index} className='square'></button>
+        )}
       </div>
     </div>
   );
@@ -16,25 +19,44 @@ function Board() {
 
 function Game() {
   const [made, setMade] = useState(false);
-  function handleMake() {
+  const [status, setStatus] = useState("");
+  const [gameid, setGameid] = useState("");
+  const [boardsta, setBoardsta] = useState<boolean[]>([false, false, false]);
+  const handleMake = () => {
     if (!made) {
       fetch('/make', {method: 'POST'})
-      .then(res => res.json())
-      .then(data => {
-        console.log(data)
+        .then(res => res.json())
+        .then(data => {
+          setStatus(data.res);
+          setGameid(data.id);
+          setBoardsta(data.board.cells);
       })
       setMade(true);
+    }
+    else {
+      const requestOption = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ id: gameid, from: 0, to: 1 })
+      }
+      fetch('/reset', requestOption)
+        .then(res => res.json())
+        .then(data => {
+          setStatus(data.res);
+          setGameid(data.id);
+          setBoardsta(data.board.cells);
+        })
     }
   }
 
   return (
     <div className='game'>
       <div className='game-board'>
-        <Board />
+        <Board boardsta={boardsta}/>
       </div>
       <div className='game-info'>
         <button onClick={handleMake}>{made ? "リセット": "作成"}</button>
-        <label >Error</label>
+        <label >{status}</label>
       </div>
     </div>
   )
