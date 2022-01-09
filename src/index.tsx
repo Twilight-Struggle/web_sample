5176a619998a8673f2d40a161f73f8f51ae2afd6
Twilight-Struggle <pikupiken@yahoo.co.jp>
2022-01-10 00:03:03 +0900

    react途中

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function Board() {
+  return (
+    <div>
+      <div className='board-row'>
+        <button className='square'></button>
+        <button className='square'></button>
+        <button className='square'></button>
+      </div>
+    </div>
+  );
+}
+
+function Game() {
+  const [made, setMade] = useState(false);
+  function handleMake() {
+    if (!made) {
+      fetch('/make', {method: 'POST'})
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+      })
+      setMade(true);
+    }
+  }
+
+  return (
+    <div className='game'>
+      <div className='game-board'>
+        <Board />
+      </div>
+      <div className='game-info'>
+        <button onClick={handleMake}>{made ? "リセット": "作成"}</button>
+        <label >Error</label>
+      </div>
+    </div>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Game />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
