104f6e23cd589eff7f7d2a6b7c94cd20903fbd79
Twilight-Struggle <pikupiken@yahoo.co.jp>
2022-01-10 18:58:34 +0900

    moveを実装

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,41 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-type BoardPorps = {boardsta: boolean[]}
+type BoardPorps = {
+  boardsta: boolean[],
+  gameid: string,
+  setStatus: React.Dispatch<React.SetStateAction<string>>,
+  setBoardsta: React.Dispatch<React.SetStateAction<boolean[]>>
+};
 function Board(props: BoardPorps) {
+  const [move, setMove] = useState(-1);
+  const handleMove = (index: number) => {
+    console.log(index);
+    if (move !== -1) {
+      const requestOption = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ id: props.gameid, from: move, to: index })
+      };
+      fetch('/mov', requestOption)
+        .then(res => res.json())
+        .then(data => {
+          props.setStatus(data.res);
+          props.setBoardsta(data.board.cells);
+        });
+      setMove(-1);
+    }
+    else {
+      setMove(index);
+    }
+  }
   return (
     <div>
       <div className='board-row'>
         {props.boardsta.map(
           (piece, index) => piece ?
-            <button key={index} className='square-piece'></button>:
-            <button key={index} className='square'></button>
+            <button key={index} onClick={(e) => handleMove(index)} className='square-piece'></button>:
+            <button key={index} onClick={(e) => handleMove(index)} className='square'></button>
         )}
       </div>
     </div>
@@ -30,7 +56,7 @@ function Game() {
           setStatus(data.res);
           setGameid(data.id);
           setBoardsta(data.board.cells);
-      })
+      });
       setMade(true);
     }
     else {
@@ -38,21 +64,25 @@ function Game() {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ id: gameid, from: 0, to: 1 })
-      }
+      };
       fetch('/reset', requestOption)
         .then(res => res.json())
         .then(data => {
           setStatus(data.res);
-          setGameid(data.id);
           setBoardsta(data.board.cells);
-        })
+        });
     }
   }
 
   return (
     <div className='game'>
       <div className='game-board'>
-        <Board boardsta={boardsta}/>
+        <Board
+          boardsta={boardsta}
+          gameid={gameid}
+          setStatus={setStatus}
+          setBoardsta={setBoardsta}
+        />
       </div>
       <div className='game-info'>
         <button onClick={handleMake}>{made ? "リセット": "作成"}</button>
